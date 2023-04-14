4fc035e5755af53ca592b1f12607177e0828cf38
Krishnaraj NeuralraysAI <68226862+krishnarajmitosis@users.noreply.github.com>
2023-04-14 21:35:45 +0530

    app bar updated

--- app/components/navbar/Navbar.tsx
+++ app/components/navbar/Navbar.tsx
@@ -1,5 +1,6 @@
 import Container from "../Container";
 import Logo from "./Logo";
+import Search from "./Search";
 
 const Navbar = () => {
   return (
@@ -16,7 +17,8 @@ const Navbar = () => {
                     md-gap-0
                     "
           >
-                <Logo />
+            <Logo />
+            <Search />
           </div>
         </Container>
       </div>
